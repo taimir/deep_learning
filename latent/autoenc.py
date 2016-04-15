41cbdf9374ed303ecd115c5902cfa20495485472
Atanas Mirchev <taimir93@gmail.com>
2016-04-15 08:15:14 +0200

    Autoencoder: first self implementation

--- latent/autoenc.py
+++ latent/autoenc.py
@@ -19,7 +19,7 @@ based on Theano
 
 
 class NeuralLayer:
-    def __init__(self, inputs, in_dim, out_dim, activation):
+    def __init__(self, inputs, in_dim, out_dim, activation=None):
         """
         A layer is defined by the number (dim) of the input units, the number of the output units
         and the activation function used by those units.
@@ -34,19 +34,23 @@ class NeuralLayer:
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.activation = activation
+        print("Creating layer with {} and {} with activation {}").format(in_dim, out_dim, activation)
 
         # define the weights for this layer
         self.W = shared(0.1 * random.randn(in_dim, out_dim), name='W', borrow=True)
         self.b = shared(np.zeros(out_dim), name="b", borrow=True)
 
         net = T.dot(inputs, self.W) + self.b
-        self.outputs = activation(net)
+        if activation is None:
+            self.outputs = net
+        else:
+            self.outputs = activation(net)
 
 
-class NeuralNetClassifier:
+class NeuralNetAutoencoder:
     MAX_EPOCHS = 100.0
 
-    def __init__(self, network_spec):
+    def __init__(self, network_spec, L1_coef=0.0001):
         """
         :param network_spec: A dictionary, which specifies only the hidden layers.
         Has the following form:
@@ -74,36 +78,19 @@ class NeuralNetClassifier:
             self.layers.append(next_layer)
 
         # Create a softmax classification output layer
-        self.output_layer = NeuralLayer(prev_out, self.layers[-1].out_dim, network_spec["out_dim"], T.nnet.softmax)
+        self.output_layer = NeuralLayer(prev_out, self.layers[-1].out_dim, network_spec["out_dim"])
         self.layers.append(self.output_layer)
 
-        # Define the cost function
-        # first we need a placeholder for the target labels
-        self.targets = T.lvector('targets')
+        # Define a L1 sparsity constraint for the parameters
+        L1 = T.mean([abs(layer.W).mean() for layer in self.layers])
+        # now we form the MSE loss. Mean so that it is not dependent on the number of samples used
+        self.loss = T.mean((self.net_input - self.output_layer.outputs)**2) + L1_coef*L1
 
-        # NLL is sum of only the log(prob) for the CORRECT class per instance
-        # we can select them by indexing the probs matrix (like in logregression)
-        filtered = self.output_layer.outputs[
-            T.arange(self.targets.shape[
-                         0]), self.targets]  # broadcasted indexes, the result is a vector of prob. for each instance
-
-        # now we form the NLL. Mean so that it is not dependent on the number of samples used
-        self.loss = -T.log(filtered).mean()
-
-        # define the error rate
-        predictions = T.argmax(self.output_layer.outputs, axis=1)  # map the softmax probabilities to a digit (0 to 9)
-        missclass_rate = T.neq(predictions, self.targets).mean()
-        self._error_rate = theano.function(inputs=[self.net_input, self.targets], outputs=missclass_rate)
+        self._error_rate = theano.function(inputs=[self.net_input], outputs=self.loss)
 
         # Build a list of gradient tuples per layer: First gradient is grad_W (matrix) and
         # second gradient is grad_b (bias)
         grads = [(T.grad(self.loss, layer.W), T.grad(self.loss, layer.b)) for layer in self.layers]
-        grads_climin = []
-        for grad_W, grad_b in grads:
-            grads_climin.append(grad_W)
-            grads_climin.append(grad_b)
-        self._flat_grad = theano.function(inputs=[self.net_input, self.targets],
-                                          outputs=grads_climin)
 
         # define the gradient descent updates
         updates = []
@@ -112,15 +99,15 @@ class NeuralNetClassifier:
             updates.append((layer.b, layer.b - 0.1 * grad_b))
 
         self.train_step = theano.function(
-            inputs=[self.net_input, self.targets],
-            outputs=missclass_rate,
+            inputs=[self.net_input],
+            outputs=self.loss,
             updates=updates
         )
 
-    def error_rate(self, X, y):
-        return self._error_rate(X, y)
+    def error_rate(self, X):
+        return self._error_rate(X)
 
-    def train(self, train_x, train_y, valid_x, valid_y, test_x, test_y, init_learning_rate=0.9, batch_size=20,
+    def train(self, train_x, valid_x, test_x, batch_size=20,
               stop_threshold=10):
         """
         Trains the logistic regression model with stochastic gradient descent.
@@ -147,10 +134,9 @@ class NeuralNetClassifier:
         counter = 0
         epoch_counter = 0
         validation_threshold = train_x.shape[0] // batch_size
-        while epoch_counter < NeuralNetClassifier.MAX_EPOCHS:
+        while epoch_counter < NeuralNetAutoencoder.MAX_EPOCHS:
             mini_batch_x = train_x[counter * batch_size: (counter + 1) * batch_size]
-            mini_batch_y = train_y[counter * batch_size: (counter + 1) * batch_size]
-            batch_err = batch_err + self.train_step(mini_batch_x, mini_batch_y)
+            batch_err = batch_err + self.train_step(mini_batch_x)
             counter += 1
             # one training epoch has passed
             if counter == validation_threshold:
@@ -159,7 +145,7 @@ class NeuralNetClassifier:
 
                 epoch_counter += 1
                 print("Epoch {} passed. Validating model:").format(epoch_counter)
-                new_err = self.error_rate(valid_x, valid_y)
+                new_err = self.error_rate(valid_x)
                 if new_err < best_err:
                     no_better_since = 0
                     best_err = new_err
@@ -173,7 +159,7 @@ class NeuralNetClassifier:
 
                 valid_errors.append(new_err)
                 train_errors.append(batch_err / validation_threshold)
-                test_errors.append(self.error_rate(test_x, test_y))
+                test_errors.append(self.error_rate(test_x))
 
                 counter = 0
                 batch_err = 0
@@ -192,50 +178,7 @@ class NeuralNetClassifier:
             layer.W.set_value(final_W)
             layer.b.set_value(final_b)
 
-        print("Final error rate: {}").format(self.error_rate(valid_x, valid_y))
-
-    def train_climin(self, train_x, train_y, valid_x, valid_y, test_x, test_y):
-        """
-        Trains the logistic regression model using the climin optimizer
-        """
-        print("Training model with climin gradient descent optimization")
-
-        def unpack_params(parameters, model):
-            current = 0
-            for layer in model.layers:
-                W = parameters[current:current + layer.in_dim * layer.out_dim].reshape(layer.in_dim, layer.out_dim)
-                b = parameters[
-                    current + layer.in_dim * layer.out_dim:current + layer.in_dim * layer.out_dim + layer.out_dim]
-                layer.W.set_value(W)
-                layer.b.set_value(b)
-                # increment index for the next layer
-                current = current + layer.in_dim * layer.out_dim + layer.out_dim
-
-        def grad_func(parameters, X, y, model):
-            unpack_params(parameters, model)
-            grads = model._flat_grad(X, y)
-            # flatten all the parameters. The order of the layers is preserved, W follows before b always
-            return np.concatenate(map(lambda grad: grad.flatten(), grads))
-
-        # initialize a flat parameters placeholder for climin
-        # randomize the parameters, but keep variance small enough (0.01) to promote faster learning
-        size = 0
-        for layer in self.layers:
-            size = size + layer.in_dim * layer.out_dim + layer.out_dim
-        params = 0.01 * random.randn(size)
-
-        # pass self during the training to update the parameters of the neural network object itself
-        # and reuse the theano definitions from the constructor
-        opt = climin.GradientDescent(params, grad_func, step_rate=0.1, momentum=0.9,
-                                     args=itertools.repeat(([train_x, train_y, self], {})))
-
-        print("Initial error rate: {}").format(self.error_rate(valid_x, valid_y))
-        for iter_info in opt:
-            print("Epoch {}").format(iter_info['n_iter'])
-            print("Error rate {}").format(self.error_rate(valid_x, valid_y))
-            if iter_info['n_iter'] >= 300:
-                break
-        print("Final error rate: {}").format(self.error_rate(valid_x, valid_y))
+        print("Final error rate: {}").format(self.error_rate(valid_x))
 
 
 """
@@ -251,8 +194,8 @@ with gzip.open('mnist.pkl.gz', 'rb') as f:
 """
 Init
 """
-nnet = NeuralNetClassifier({
-    "out_dim": 10,
+nnet = NeuralNetAutoencoder({
+    "out_dim": train_x.shape[1],
     "in_dim": train_x.shape[1],
     "layer_specs": [
         {"in_dim": train_x.shape[1], "out_dim": 300, "activation": T.tanh}
@@ -260,19 +203,6 @@ nnet = NeuralNetClassifier({
 })
 
 """
-Train
+Train autoencoder
 """
-nnet.train_climin(train_x, train_y, valid_x, valid_y, test_x, test_y)
-
-"""
-Perform on test data
-"""
-test_error = nnet.error_rate(test_x, test_y)
-print("Achieved error rate on test set: {}").format(test_error)
-
-"""
-Receptive fields
-"""
-for i in range(10):
-    plt.imshow(nnet.layers[0].W.eval()[:, i].reshape((28, 28)), cmap=cm.Greys_r)
-    plt.show()
+nnet.train(train_x, valid_x, test_x)
--- nn/theano_nn.py
+++ nn/theano_nn.py
@@ -402,7 +402,7 @@ class MLP(object):
         self.input = input
 
 
-def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
+def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=110,
              dataset='mnist.pkl.gz', batch_size=20, n_hidden=300):
     """
     Demonstrate stochastic gradient descent optimization for a multilayer
